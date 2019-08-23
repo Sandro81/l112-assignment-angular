2c6c764f615d3def5994e76f945a19340cac6709
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-23 14:13:04 +0100

    1 file modified

--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -10,13 +10,13 @@ import { UsersServiceService } from '../services/users-service.service'
 export class InactiveUsersComponent implements OnInit {
   users: string[];
 
-  constructor(private usersService: UsersServiceService) {}
+  constructor(private userService: UsersServiceService) {}
 
   ngOnInit () {
-    this.users = this.usersService.activeUsers;
+    this.users = this.userService.activeUsers;
   }
 
   onSetToActive(id: number) {
-    this.usersService = this.onSetToActive(id);
+    this.userService = this.onSetToActive(id);
   }
 }
