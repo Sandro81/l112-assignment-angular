6b3d39c79b0520819823494ece7d056d266cede7
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-23 12:23:52 +0100

    2 files modified

--- src/app/services/users-service.service.ts
+++ src/app/services/users-service.service.ts
@@ -8,4 +8,14 @@ export class UsersServiceService {
 
   constructor() { }
 
+  setToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
+
+  setToInactive(id: number){
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
+
 }
