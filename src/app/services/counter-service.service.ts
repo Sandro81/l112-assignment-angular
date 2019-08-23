724453196386c99e8ae53bcf168c1742ad1bd76e
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-23 14:31:31 +0100

    2 files modified

--- src/app/services/counter-service.service.ts
+++ src/app/services/counter-service.service.ts
@@ -3,6 +3,20 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CounterServiceService {
 
+activeToInactiveCounter = 0;
+inactiveToActiveCounter = 0;
+
   constructor() { }
+
+  incrementActiveToInactive() {
+    this.activeToInactiveCounter++;
+    console.log(this.activeToInactiveCounter);
+  }
+
+
+  incrementInactiveToActive() {
+    this.inactiveToActiveCounter++;
+    console.log(this.inactiveToActiveCounter);
+  }
 
 }
--- src/app/services/users-service.service.ts
+++ src/app/services/users-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CounterServiceService } from '../services/counter-service.service';
 
 @Injectable()
 export class UsersServiceService {
@@ -6,16 +7,18 @@ export class UsersServiceService {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
-  constructor() { }
+  constructor(private counterService: CounterServiceService) { }
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.counterService.incrementInactiveToActive();
   }
 
   setToInactive(id: number){
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counterService.incrementActiveToInactive();
   }
 
 }
